b3a0b2e99c2ead1431789d0030cfaa7de9ff939a
bealmeidaDev <beatrizADev@gmail.com>
2023-11-21 15:58:26 -0300

    nav dinamico

---
+++ javascript/nave.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector("nav");
+
+nav.innerHTML = 
+ `
+ <ul>
+  <li><a href="/index.html">Início</a></li>
+  <li><a href="/html/grideumacoluna.html"> - Uma Coluna</a></li>
+  <li><a href="/html/gridevariascolunas.html"> - várias colunas </a></li>
+  <li><a href="/html/gridColunasAdaptaveis.html">Grid Com gridColunasAdaptaveis</a></li>
+</ul>
+`;
+
